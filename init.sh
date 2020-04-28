7503516ac753ad748f380a7a5b134a7760e4279b
Zach Shipko <zshipko@recurly.com>
2020-04-27 19:26:54 -0700

    update

--- init.sh
+++ init.sh
@@ -10,14 +10,4 @@ mv ocaml-rust-starter.opam $public_name.opam
 mv src/ocaml_rust_starter.ml src/$name.ml
 mv src/ocaml_rust_starter.mli src/$name.mli
 echo "" > test/test.ml
-rm -rf .git
-rm init.sh
-git init
-git add $public_name.opam
-git add src/$name.ml src/$name.mli src/lib.rs src/dune
-git add test/dune test/test.ml
-git add dune-project
-git add Cargo.toml
-git add LICENSE.md
-git add README.md
 
