f59b9e1e6756b0e4697fbe114df6b7bc47605ef4
Zach Shipko <zshipko@recurly.com>
2020-04-27 19:19:21 -0700

    update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,23 @@
 name=$1
 public_name=${2-$name}
-sed -i 's/ocaml_rust_starter/'"$name"'/g' src/dune
-sed -i 's/ocaml-rust-starter/'"$public_name"'/g' src/dune
-sed -i 's/ocaml-rust-starter/'"$public_name"'/g' test/dune
-sed -i 's/ocaml-rust-starter/'"$public_name"'/g' Cargo.toml
+sed -i "" 's/ocaml-rust-starter/'"$public_name"'/g' dune-project
+sed -i "" 's/ocaml_rust_starter/'"$name"'/g' src/dune
+sed -i "" 's/ocaml-rust-starter/'"$public_name"'/g' src/dune
+sed -i "" 's/ocaml-rust-starter/'"$public_name"'/g' test/dune
+sed -i "" 's/ocaml-rust-starter/'"$public_name"'/g' Cargo.toml
 printf "# $public_name\n" > README.md
 mv ocaml-rust-starter.opam $public_name.opam
-mv src/ocaml_rust_starter.ml src/$1.ml
-mv src/ocaml_rust_starter.mli src/$1.mli
+mv src/ocaml_rust_starter.ml src/$name.ml
+mv src/ocaml_rust_starter.mli src/$name.mli
 echo "" > test/test.ml
-#rm -rf .git
+rm -rf .git
 rm init.sh
+git init
+git add $public_name.opam
+git add src/$name.ml src/$name.mli src/lib.rs src/dune
+git add test/dune test/test.ml
+git add dune-project
+git add Cargo.toml
+git add LICENSE.md
+git add README.md
+
