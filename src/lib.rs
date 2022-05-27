c047fd2dbbc20d2b3793acdb7f71cf9431dd1f9f
zach <zachshipko@gmail.com>
2022-05-27 10:35:29 -0700

    Use ocaml::sig + ocaml-build

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #[ocaml::func]
+#[ocaml::sig("unit -> string")]
 pub fn hello_world() -> &'static str {
     "hello, world!"
 }
--- src/ocaml_rust_starter.ml
+++ src/ocaml_rust_starter.ml
@@ -1,1 +1,2 @@
+open! Bigarray
 external hello_world: unit -> string = "hello_world"
